469447cddbea310553e0c6941341537ad71a5b87
tijes <tijes@DESKTOP-D6TFKBH>
2025-12-12 16:11:53 +0100

    Add Controllers

---
+++ MyPractice/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPractice.Models.Entities;
+
+namespace MyPractice.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<MyPrac> MyPracs { get; set; }
+    }
+}
