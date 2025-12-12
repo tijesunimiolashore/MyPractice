d4dd939ad6333dbf0abd507989c43b02860c2bab
tijes <tijes@DESKTOP-D6TFKBH>
2025-12-12 15:02:25 +0100

    Add MyPractice Entity Class

---
+++ MyPractice/Models/Entities/MyPrac.cs
@@ -0,0 +1,15 @@
+﻿namespace MyPractice.Models.Entities
+{
+    public class MyPrac
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+		public decimal Salary { get; set; }
+	}
+}
