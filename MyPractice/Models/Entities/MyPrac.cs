d680cbabb1fb02fc29d1ddbec0046cf52e97427b
tijes <tijes@DESKTOP-D6TFKBH>
2025-12-16 22:20:14 +0100

    Added My Recent Model and Entity Folders

--- MyPractice/Models/Entities/MyPrac.cs
+++ MyPractice/Models/Entities/MyPrac.cs
@@ -4,11 +4,11 @@
     {
         public Guid Id { get; set; }
 
-        public string FirstName { get; set; }
+        public required string FirstName { get; set; }
         
-        public string LastName { get; set; }
+        public required string LastName { get; set; }
 
-        public string PhoneNumber { get; set; }
+        public required string PhoneNumber { get; set; }
 
 		public decimal Salary { get; set; }
 	}
---
+++ MyPractice/Models/MyPracticeDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MyPractice.Models
+{
+    public class MyPracticeDto
+    {
+		public required string FirstName { get; set; }
+
+		public required string LastName { get; set; }
+
+		public required string PhoneNumber { get; set; }
+
+		public decimal Salary { get; set; }
+	}
+}
